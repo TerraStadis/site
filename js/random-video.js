86e09fe264c226a2da1c42eed35c61dd33fa7b9a
Terra Stadis <87304359+TerraStadis@users.noreply.github.com>
2025-06-30 01:51:56 +0800

    Update random-video.js

--- js/random-video.js
+++ js/random-video.js
@@ -8,8 +8,7 @@ const videos = [
   "videos/Лайкер и Гавер.mp4",
   "videos/МАШИНА ВДРЕБЕЗГИ.mp4",
   "videos/отпуска нет ультракалл.mp4",
-  "videos/Frostpunk.mp4",
-  "videos/Метель.mp4"
+  "videos/Frostpunk.mp4"
 ];
 
 const randomIndex = Math.floor(Math.random() * videos.length);
