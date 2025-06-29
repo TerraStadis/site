53b14cc268c523ed79c0f1e07eabb00f71f71cc1
Terra Stadis <87304359+TerraStadis@users.noreply.github.com>
2025-06-30 01:31:10 +0800

    Add files via upload

---
+++ js/random-video.js
@@ -0,0 +1,16 @@
+const videoElement = document.getElementById("bg-video");
+
+const videos = [
+  "videos/Happppy.mp4",
+  "videos/Перекрас для Никиты.mp4",
+  "videos/главный алмазик итого 3.mp4",
+  "videos/Нави.mp4",
+  "videos/Лайкер и Гавер.mp4",
+  "videos/МАШИНА ВДРЕБЕЗГИ.mp4",
+  "videos/отпуска нет ультракалл.mp4",
+  "videos/Frostpunk.mp4",
+  "videos/Метель.mp4"
+];
+
+const randomIndex = Math.floor(Math.random() * videos.length);
+videoElement.src = videos[randomIndex];
